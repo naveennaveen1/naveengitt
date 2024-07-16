54b9a8acf4ae6438d77f26bcd35962ba0030bda2
naveennaveen1 <naveennrockey1926@gmail.com>
2024-07-16 23:37:03 +0530

    added Hello naveen

---
+++ javatask.java
@@ -0,0 +1,15 @@
+package naveengitt;
+
+public class javatask{
+public static void main(String [] args){
+  System.out.println("Hello java");
+  
+  //naveen branch created
+  System.out.println("Hello naveen");
+  
+  
+  
+  
+  
+}
+}
